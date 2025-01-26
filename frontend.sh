71416e0daa1d0bbbad5d9abb624ef14a7bedc10b
GOUTHAM <goutamreddy817@gmail.com>
2025-01-26 20:54:23 +0530

    prac

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,25 @@
-dnf module disable nginx -y
+dnf module disable nginx -y &>>/tmp/roboshop.log
+echo $?
 dnf module enable nginx:1.24 -y
+echo $?
 
 echo -e "\e[35m install nginx \e[0m"
 dnf install nginx -y &>>/tmp/roboshop.log
+  if [ $1 -eq 0]; then
+     echo -e "\e[32m sucess \e[0m"
+   else
+     echo -e "\e[32m failed \e[0m"
+  if
 
 cp nginx.conf /etc/nginx/nginx.conf
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+echo $?
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
+echo $?
 
 systemctl enable nginx
 systemctl restart nginx
+echo $?
