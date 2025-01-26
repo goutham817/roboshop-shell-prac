59163c3ebc3c98e06e59d4c08e1882df8f337707
GOUTHAM <goutamreddy817@gmail.com>
2025-01-26 21:13:14 +0530

    prac

--- frontend.sh
+++ frontend.sh
@@ -18,5 +18,5 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
 
-systemctl restart nginx
+systemctl start nginx
 systemctl enable nginx
