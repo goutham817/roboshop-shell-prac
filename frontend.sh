be7c2537100c683d747e3f797df5f6f4ea25ed46
GOUTHAM <goutamreddy817@gmail.com>
2025-01-28 20:06:42 +0530

    prac

--- frontend.sh
+++ frontend.sh
@@ -5,12 +5,11 @@ echo $?
 
 echo -e "\e[35m install nginx \e[0m"
 dnf install nginx -y &>>/tmp/roboshop.log
-  if [ $1 -eq 0]; then
+  if [ $1 -eq 0 ]; then
      echo -e "\e[32m sucess \e[0m"
    else
      echo -e "\e[32m failed \e[0m"
-  if
-  echo $?
+  fi
 
 cp nginx.conf /etc/nginx/nginx.conf &>>/tmp/roboshop.log
 
@@ -19,25 +18,24 @@ rm -rf /usr/share/nginx/html/*
 echo -e "\e[35m downloaded content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
- if [ $1 -eq 0]; then
+ if [ $1 -eq 0 ]; then
    echo -e "\e[32m sucess \e[0m"
    else
      echo -e "\e[31m failure \e[0m"
      fi
-     echo $?
 
 echo -e "\e[35m restart nginx \e[0m"
 systemctl restart nginx &>>/tmp/roboshop.log
-if [ $1 -eq 0]; then
+if [ $1 -eq 0 ]; then
    echo -e "\e[32m sucess \e[0m"
    else
    echo -e "\e[31m failure \e[0m"
      fi
-     echo $?
 
 echo -e "\e[35m enable nginx \e[0m"
 systemctl enable nginx
-if [ $1 -eq 0]; then
+if [ $1 -eq 0 ]; then
   echo -e "\e[32m sucess \e[0m"
   else
   echo -e "\e[31m failure \e[0m"
+  fi
